ebdcf8f2196069f523786449298c8fc8b33d32ac
Karim Alibhai <k4rim.sa@gmail.com>
2014-12-10 14:47:53 -0500

    clean up javascript

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
-window.stat = function (txt, cb) {
-    txt = txt || '';
-    cb = cb || function () {};
+/*jslint browser:true*/
+/*globals msg, cont, stat, message*/
+(function () {
+    "use strict";
 
-    msg.style.opacity = 0;
-    setTimeout(function () {
-        msg.innerHTML = txt;
-        setTimeout(function () {
-            msg.style.opacity = 1;
-            setTimeout(cb, 401);
-        }, 200);
-    }, 401);
-};
+    var noop = function () {
+            return false;
+        },
+        exports = {
+            msg: window.msg || document.getElementById('msg'),
+            cont: window.cont || document.getElementById('cont'),
 
-window.message = function (txt, stay) {
-    stay = stay === undefined ? true : !!stay;
-    txt = String(txt).split(' ').concat(stay?['&nbsp;&nbsp;&nbsp;&nbsp;']:[]);
-    var i = -1;
-    var next = function () {
-        i += 1;
-        cont.style.opacity = 1;
-        if (i < txt.length) {
-            stat(txt[i], next);
-        } else if (!stay) {
-            cont.style.opacity = 0;
+            stat: function (text, callback) {
+                callback = callback || noop;
+                text = String(text);
+
+                msg.style.opacity = 0;
+                setTimeout(function () {
+                    msg.innerHTML = text;
+                    setTimeout(function () {
+                        msg.style.opacity = 1;
+                        setTimeout(callback, 401);
+                    }, 200);
+                }, 401);
+            },
+
+            message: function (text, stay) {
+                stay = stay === undefined ? true : !!stay;
+                text = String(text).split(' ').concat(stay ? ['&nbsp;&nbsp;&nbsp;&nbsp;'] : []);
+
+                var i = -1,
+                    next = function () {
+                        i += 1;
+                        cont.style.opacity = 1;
+
+                        if (i < text.length) {
+                            stat(text[i], next);
+                        } else if (!stay) {
+                            cont.style.opacity = 0;
+                        }
+                    };
+
+                next();
+            }
+        },
+        i;
+
+    for (i in exports) {
+        if (exports.hasOwnProperty(i)) {
+            window[i] = exports[i];
         }
-    };
-
-    next();
-};
+    }
+}());
